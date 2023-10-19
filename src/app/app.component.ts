853e08018efadad8eddcd6d4d20a2afb49e07cee
Belin27000 <y.lecerf@protonmail.com>
2023-10-19 17:39:27 +0200

    feat: add *ngfor on faceSnap card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,60 @@ import { FaceSnap } from './models/face-snaps-model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap
-  myLastSnap!: FaceSnap
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    )
-    this.myOtherSnap = new FaceSnap(
-      'Yann',
-      'Mes plus belle vacances',
-      new Date(),
-      10,
-      'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg'
-    )
-    this.myLastSnap = new FaceSnap(
-      'Camille',
-      'Je ne sais pas pourquoi il y a cette photo',
-      new Date(),
-      150,
-      'https://cdn.pixabay.com/user/2015/10/16/09-28-45-303_250x250.png'
-    )
+    this.faceSnaps = [
+
+      {
+
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location: 'Paris'
+      },
+      {
+        title: 'Yann',
+        description: 'Mes plus belle vacances',
+        createdDate: new Date(),
+        snaps: 10,
+        imageUrl: 'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
+        location: 'La montagne'
+      },
+      {
+        title: 'Camille',
+        description: 'Je ne sais pas pourquoi il y a cette photo',
+        createdDate: new Date(),
+        snaps: 90,
+        imageUrl: 'https://cdn.pixabay.com/user/2015/10/16/09-28-45-303_250x250.png',
+      },
+      {
+
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location: 'Paris'
+      },
+      {
+        title: 'Yann',
+        description: 'Mes plus belle vacances',
+        createdDate: new Date(),
+        snaps: 10,
+        imageUrl: 'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
+        location: 'La montagne'
+      },
+      {
+        title: 'Camille',
+        description: 'Je ne sais pas pourquoi il y a cette photo',
+        createdDate: new Date(),
+        snaps: 90,
+        imageUrl: 'https://cdn.pixabay.com/user/2015/10/16/09-28-45-303_250x250.png',
+      }
+    ]
   }
 
 }
