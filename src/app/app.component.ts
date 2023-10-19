9e6c6b2f107b73ca1a2e2190a5046fa9042ab777
Belin27000 <y.lecerf@protonmail.com>
2023-10-19 21:33:43 +0200

    feat: add percent pipe and number pipe + service implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snaps-model';
 
 @Component({
   selector: 'app-root',
@@ -7,60 +6,7 @@ import { FaceSnap } from './models/face-snaps-model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
 
-  ngOnInit() {
-    this.faceSnaps = [
-
-      {
-
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit',
-        createdDate: new Date(),
-        snaps: 210,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        location: 'Paris'
-      },
-      {
-        title: 'Yann',
-        description: 'Mes plus belle vacances',
-        createdDate: new Date(),
-        snaps: 150,
-        imageUrl: 'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
-        location: 'La montagne'
-      },
-      {
-        title: 'Camille',
-        description: 'Je ne sais pas pourquoi il y a cette photo',
-        createdDate: new Date(),
-        snaps: 90,
-        imageUrl: 'https://cdn.pixabay.com/user/2015/10/16/09-28-45-303_250x250.png',
-      },
-      {
-
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit',
-        createdDate: new Date(),
-        snaps: 0,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        location: 'Paris'
-      },
-      {
-        title: 'Yann',
-        description: 'Mes plus belle vacances',
-        createdDate: new Date(),
-        snaps: 10,
-        imageUrl: 'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
-        location: 'La montagne'
-      },
-      {
-        title: 'Camille',
-        description: 'Je ne sais pas pourquoi il y a cette photo',
-        createdDate: new Date(),
-        snaps: 90,
-        imageUrl: 'https://cdn.pixabay.com/user/2015/10/16/09-28-45-303_250x250.png',
-      }
-    ]
-  }
+  ngOnInit() { }
 
 }
